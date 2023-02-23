3a0f339971a596c25c51b363db0a99c4e023acd0
Laketequin1 <laketequin1@gmail.com>
2023-02-23 13:29:09 +1300

    Got a screen working, and an image displaying.

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import pygame, os, sys
 pygame.init()
 
 # Imports lots of colors as RGB
-from src import color
+from src import color as Color
 
 os.system("cls")
 
 # ----- Constant Variables -----
-FPS = 60
+FPS = 120
 
 GAME_WIDTH = 1920
 GAME_HEIGHT = 1080
 
 class Sprite:
-    class player:
-        f0 = pygame.image.load("images/player/frame0.png")
-    pass
-
+    class Player:
+        f0 = pygame.image.load("images/player/f0.png")
+        
 # ----- Variables -----
 clock = pygame.time.Clock()
 
 # ----- Function ------
 def exit():
+    # Exit server here
     pygame.quit()
     sys.exit()
     
@@ -33,11 +33,43 @@ class Render:
     DISPLAY_WIDTH = info.current_w
     DISPLAY_HEIGHT = info.current_h
     
-    print(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    BACKGROUND_COLOR = Color.BLACK
+    
+    queued_images = []
     
     def __init__(self, game_resolution: tuple[int, int]):
-        self.display = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT), pygame.NOFRAME)
+        self.screen = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT), pygame.NOFRAME)
+        pygame.display.set_caption("Shadow Fare")
         self.game_resolution = game_resolution
         
         self.WIDTH_MULTIPLIER = self.DISPLAY_WIDTH/GAME_WIDTH
         self.HEIGHT_MULTIPLIER = self.DISPLAY_HEIGHT/GAME_HEIGHT
+
+    def blit(self, *image):
+        self.queued_images.append(image)
+    
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                exit()
+    
+    def display(self):
+        self.screen.fill(self.BACKGROUND_COLOR)
+        for i, image in enumerate(self.queued_images):
+            self.screen.blit(*image)
+        pygame.display.update()
+        self.queued_images = []
+        
+    def tick(self):
+        clock.tick(FPS)
+            
+            
+render = Render((GAME_WIDTH, GAME_WIDTH))
+
+running = True
+while running:
+    render.update()
+    render.blit(Sprite.Player.f0, (0, 0))
+    render.display()
